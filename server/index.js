44debba00c149510377254ed53ffa3916634c269
Michael Dale <vyridian@gmail.com>
2022-01-07 14:38:20 +0900

    Update: Refactoring for new deployment

--- server/index.js
+++ server/index.js
@@ -1,39 +1,42 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import itemRoutes from './routes/item.js';
-import userRoutes from './routes/user.js';
-import stripeRoutes from './routes/stripe.js';
-import orderRoutes from './routes/order.js';
+import itemRoutes from "./routes/item.js";
+import userRoutes from "./routes/user.js";
+import stripeRoutes from "./routes/stripe.js";
+import orderRoutes from "./routes/order.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // just to say hello from server app launch
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
-const password = process.env.DB_PASS
-const CONNECTION_URL = "mongodb+srv://renty:" + password + "@cluster0.trccm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const password = process.env.DB_PASS;
+const CONNECTION_URL =
+  "mongodb+srv://renty:" +
+  password +
+  "@cluster0.trccm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 4000;
 
 mongoose
   .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
   })
   .then(() =>
-    app.listen(PORT, () => {
-      `Listening: http://localhost:${PORT}`;
-      console.log(`Connected to Port: ${PORT}`)
+    app.listen(PORT, (req, res) => {
+      console.log(`Listening: http://localhost:${PORT}`);
+      console.log(`Connected to Port: ${PORT}`);
     })
   )
   .catch((err) => console.log(err.message));
@@ -41,7 +44,7 @@ mongoose
 mongoose.set("useFindAndModify", false);
 
 //Routes
-app.use('/items', itemRoutes)
-app.use('/users', userRoutes)
-app.use('/stripe', stripeRoutes)
-app.use('/order', orderRoutes)
+app.use("/items", itemRoutes);
+app.use("/users", userRoutes);
+app.use("/stripe", stripeRoutes);
+app.use("/order", orderRoutes);
